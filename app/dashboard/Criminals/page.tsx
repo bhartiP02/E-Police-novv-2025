16dd65a7bed234198e9f77b6b0799d5f52ef9c1a
Bharti Patle <bhartipatle200@gmail.com>
2025-12-02 11:15:03 +0530

    fix the issues

---
+++ app/dashboard/Criminals/page.tsx
@@ -0,0 +1,8 @@
+export default function CriminalsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-2xl font-bold mb-4">Criminals</h1>
+      <p className="text-white-600">This is the Criminals page.</p>
+    </div>
+  );
+}
